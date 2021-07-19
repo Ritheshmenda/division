cd74741559df7a483375e5f3fc32321861dd1960
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-19 23:11:31 +0530

    added:multiplication

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double x = 20;
+            Double y = 30;
+            Console.WriteLine(x / y);
+        }
+    }
+}
